3de7008fa138dc19663a1015d2c9686624625c28
hailey phan <haileyphan@haileys-MacBook-Air.local>
2026-05-20 14:09:06 -0700

    Added previous class project folders

---
+++ Class04/class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "BIMM 143 Lab 04"
+#' author: "Hailey Phan (PID: A19113445)"
+#' date: "Apr 9th, 2026"
+#' ---
+
+# this is a text file
+x <- 1:50
+plot (x,sin(x), type="l", col="pink", lwd=3)
+
+
